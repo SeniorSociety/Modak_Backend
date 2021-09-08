1de0e44baa845bdbab34242148d9794906eff973
jun <wkdghwns91@gmail.com>
2021-09-08 23:48:51 +0900

    Add: 갤러리 북마크 기능 - 북마크 등록/삭제 - 마이페이지 북마크 리스트

--- galleries/views.py
+++ galleries/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http        import JsonResponse
+from django.views       import View
 
-# Create your views here.
+from galleries.models   import Gallery, Bookmark
+from users.utils        import login_decorator
+
+class BookmarkView(View):
+    @login_decorator
+    def post(self, request, gallery_id):
+        if not Gallery.objects.filter(id=gallery_id).exists():
+            return JsonResponse({'MESSAGE' : 'GALLERY_DOES_NOT_EXIST'}, status=400)
+
+        bookmark, is_bookmark = Bookmark.objects.get_or_create(
+            gallery = Gallery.objects.get(id=gallery_id),
+            user    = request.user
+        )
+
+        if not is_bookmark:
+            bookmark.delete()
+            return JsonResponse({'MESSAGE' : 'BOOKMARK_DELETED'}, status=204)
+
+        return JsonResponse({'MESSAGE' : 'BOOKMARK_CREATED'}, status=201)
+
+    @login_decorator
+    def get(self, request):
+        bookmarks = Bookmark.objects.select_related('gallery').filter(user_id=request.user.id)
+
+        gallery_list = [{
+            'gallery_name' : bookmark.gallery.name,
+            'gallery_image' : bookmark.gallery.image
+        } for bookmark in bookmarks ]
+
+        return JsonResponse({'LIST' : gallery_list}, status=200)
