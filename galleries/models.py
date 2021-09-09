184353c7bb29ee335b950439228541c6c34f4778
jun <wkdghwns91@gmail.com>
2021-09-09 15:31:23 +0900

    Modify: modeling 수정

--- galleries/models.py
+++ galleries/models.py
@@ -5,6 +5,7 @@ from core.models import TimeStampModel
 class Gallery(models.Model):
     name  = models.CharField(max_length=30)
     image = models.URLField(max_length=2000)
+    bookmark = models.ManyToManyField('users.user', related_name='bookmarkers')
 
     class Meta:
         db_table = 'galleries'
@@ -27,7 +28,3 @@ class Comment(TimeStampModel):
 
     class Meta:
         db_table = 'comments'
-
-class Bookmark(models.Model):
-    user    = models.ForeignKey('users.user', on_delete=models.CASCADE)
-    gallery = models.ForeignKey('gallery', on_delete=models.CASCADE)
