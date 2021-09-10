af425746ffeb554b3f50bb8681214c583e721811
dwenpark <dwenup@gmail.com>
2021-09-10 11:26:52 +0900

    Add: 게시글 리스트, 게시글, 댓글 CRUD
    - 게시글 리스트
    - 게시글 보기, 쓰기, 수정, 삭제
    - 댓글 보기, 쓰기, 수정, 삭제
    - 테스트 코드 작성

--- galleries/tests.py
+++ galleries/tests.py
@@ -1,18 +1,17 @@
-from my_settings import ALGORITHMS, SECRET_KEY
 import jwt
+import json
 
 from django.test        import TestCase, Client
 
 from users.models       import User
-from galleries.models   import Gallery, Bookmark
+from galleries.models   import Gallery, Posting, Comment, Bookmark
 from my_settings        import SECRET_KEY, ALGORITHMS
 
 
 class BookmarkTest(TestCase):
     @classmethod
     def setUpTestData(self):
-        User.objects.create(
-            id          = 1,
+        user = User.objects.create(
             nickname    = 'Jun',
             kakao       = '101',
             naver       = '102',
@@ -24,29 +23,29 @@ class BookmarkTest(TestCase):
         )
 
         Gallery.objects.create(
-            id    = 1,
             name  = '경제',
             image = 'abc.jpg'
         )
 
         Bookmark.objects.create(
-            id      = 1,
-            user    = User.objects.get(id=1),
-            gallery = Gallery.objects.get(id=1)
+            user    = User.objects.get(nickname="Jun"),
+            gallery = Gallery.objects.get(name="경제")
         )
 
-        self.token = jwt.encode({'id' : User.objects.get(id=1).id}, SECRET_KEY, algorithm=ALGORITHMS)
+        self.token = jwt.encode({'id' : user.id}, SECRET_KEY, algorithm=ALGORITHMS)
 
     def tearDown(self):
         Gallery.objects.all().delete()
         User.objects.all().delete()
+        Bookmark.objects.all().delete()
 
     def test_bookmark_create_success(self):
         Bookmark.objects.all().delete()
-
-        client = Client()
-        header = {'HTTP_Authorization' : self.token}
-        response = client.post('/gallery/1/bookmark', **header, content_type='application/json')
+        
+        client     = Client()
+        gallery_id = Gallery.objects.get(name="경제").id
+        header     = {'HTTP_Authorization' : self.token}
+        response   = client.post(f'/galleries/{gallery_id}/bookmark', **header, content_type='application/json')
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json(),
@@ -54,15 +53,325 @@ class BookmarkTest(TestCase):
         )
 
     def test_bookmark_delete_success(self):
-        client = Client()
-        header = {'HTTP_Authorization' : self.token}
-        response = client.post('/gallery/1/bookmark', **header, content_type='application/json')
+        client   = Client()
+        gallery_id = Gallery.objects.get(name = "경제").id
+        header   = {'HTTP_Authorization' : self.token}
+        response = client.post(f'/galleries/{gallery_id}/bookmark', **header, content_type='application/json')
 
         self.assertEqual(response.status_code, 204)
 
     def test_bookmark_list(self):
-        client = Client()
-        header = {'HTTP_Authorization' : self.token}
-        response = client.get('/gallery/bookmark-list', **header, content_type='application/json')
+        client   = Client()
+        header   = {'HTTP_Authorization' : self.token}
+        response = client.get('/galleries/bookmark-list', **header, content_type='application/json')
 
         self.assertEqual(response.status_code, 200)
+
+class PostingListTest(TestCase):
+    @classmethod
+    def setUpTestData(self):
+        gallery = Gallery.objects.create(
+            name  = "test",
+            image = "image.jpg"
+        )
+
+        User.objects.bulk_create([
+            User(
+                nickname    = 'Jun',
+                kakao       = '101',
+                naver       = '102',
+                image       = 'Jun.jpg',
+                name        = 'HoJun Jang',
+                age         = 26,
+                description = 'Hello!',
+                namecard    = 'abcdefg'),
+            User(
+                nickname    = 'Min',
+                kakao       = '201',
+                naver       = '202',
+                image       = 'Min.jpg',
+                name        = 'Min Choi',
+                age         = 26,
+                description = 'Hello!!',
+                namecard    = 'abcdefg')
+            ]
+        )
+
+        Posting.objects.bulk_create([
+            Posting(
+                gallery = gallery,
+                title   = "testpost1",
+                content = "testtext1",
+                user    = User.objects.get(nickname = "Jun")
+            ),
+            Posting(
+                gallery = gallery,
+                title   = "testpost2",
+                content = "testtext2",
+                user    = User.objects.get(nickname = "Jun")
+            ),
+            Posting(
+                gallery = gallery,
+                title   = "testpost3",
+                content = "testtext3",
+                user    = User.objects.get(nickname = "Jun")
+            ),
+            Posting(
+                gallery = gallery,
+                title   = "testpost3",
+                content = "testtext3",
+                user    = User.objects.get(nickname = "Min")
+            )
+        ])
+
+        Comment.objects.bulk_create([
+            Comment(
+                user    = User.objects.get(nickname = "Min"),
+                posting = Posting.objects.get(title = "testpost1")
+            ),
+            Comment(
+                user    = User.objects.get(nickname = "Jun"),
+                posting = Posting.objects.get(title = "testpost1")
+            ),
+            Comment(
+                user    = User.objects.get(nickname = "Jun"),
+                posting = Posting.objects.get(title = "testpost2")
+            ),
+            Comment(
+                user    = User.objects.get(nickname = "Jun"),
+                posting = Posting.objects.get(title = "testpost2")
+            ),
+        ])
+        
+        
+    def tearDown(self):
+        Comment.objects.all().delete()
+        Posting.objects.all().delete()
+        User.objects.all().delete()
+        Gallery.objects.all().delete()
+
+    def test_posting_list_success(self):
+        client     = Client()
+        gallery_id = Gallery.objects.get(name="test").id
+        response   = client.get(f"/galleries/{gallery_id}?page=1")
+        self.assertEqual(response.status_code, 200)
+
+    def test_posting_post_success(self):
+        client       = Client()
+        user         = User.objects.get(nickname = "Jun").id
+        access_token = jwt.encode({'id' : user}, SECRET_KEY, algorithm = ALGORITHMS)
+        headers      = {"HTTP_Authorization": access_token}
+        gallery      = Gallery.objects.all()[0]
+        body         = {"title" : "1234", "content" : "4123"}
+
+        response   = client.post(f"/galleries/{gallery.id}", json.dumps(body), content_type='application/json', **headers)
+        self.assertEqual(response.status_code, 201)
+
+class PostingTest(TestCase) :
+    @classmethod
+    def setUpTestData(self) :
+        gallery = Gallery.objects.create(
+            name  = "test",
+            image = "image.jpg"
+        )
+
+        User.objects.bulk_create([
+            User(nickname = "testuser1"),
+            User(nickname = "testuser2")
+        ]
+        )
+
+        Posting.objects.bulk_create([
+            Posting(
+                gallery = gallery,
+                title   = "testpost1",
+                content = "testtext1",
+                user    = User.objects.get(nickname = "testuser1")
+            ),
+            Posting(
+                gallery = gallery,
+                title   = "testpost2",
+                content = "testtext2",
+                user    = User.objects.get(nickname = "testuser1")
+            ),
+            Posting(
+                gallery = gallery,
+                title   = "testpost3",
+                content = "testtext3",
+                user    = User.objects.get(nickname = "testuser1")
+            ),
+            Posting(
+                gallery = gallery,
+                title   = "testpost3",
+                content = "testtext3",
+                user    = User.objects.get(nickname = "testuser2")
+            )
+        ])
+
+        Comment.objects.bulk_create([
+            Comment(
+                user    = User.objects.get(nickname = "testuser2"),
+                posting = Posting.objects.get(title = "testpost1")
+            ),
+            Comment(
+                user    = User.objects.get(nickname = "testuser1"),
+                posting = Posting.objects.get(title = "testpost1")
+            ),
+            Comment(
+                user    = User.objects.get(nickname = "testuser1"),
+                posting = Posting.objects.get(title = "testpost2")
+            ),
+            Comment(
+                user    = User.objects.get(nickname = "testuser1"),
+                posting = Posting.objects.get(title = "testpost2")
+            ),
+        ])
+
+    def tearDown(self) :
+        Comment.objects.all().delete()
+        Posting.objects.all().delete()
+        User.objects.all().delete()
+        Gallery.objects.all().delete()
+
+    def test_posting_read_success(self):
+        client     = Client()
+        gallery_id = Gallery.objects.all()[0].id
+        posting_id = Posting.objects.all()[0].id
+        response   = client.get(f"/galleries/{gallery_id}/{posting_id}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_posting_patch_success(self) :
+        client       = Client()
+        user_id      = User.objects.all()[0].id
+        access_token = jwt.encode({'id' : user_id}, SECRET_KEY, algorithm = ALGORITHMS)
+        headers      = {"HTTP_Authorization" : access_token}
+        gallery      = Gallery.objects.all()[0]
+        posting      = Posting.objects.all()[0]
+        body         = {"title" : "title", "content" : "4124"}
+        response     = client.patch(f"/galleries/{gallery.id}/{posting.id}", json.dumps(body),
+                               content_type = 'application/json', **headers)
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_posting_delete_success(self) :
+        client       = Client()
+        user_id      = User.objects.all()[0].id
+        access_token = jwt.encode({'id' : user_id}, SECRET_KEY, algorithm = ALGORITHMS)
+        headers      = {"HTTP_Authorization" : access_token}
+        gallery      = Gallery.objects.all()[0]
+        posting      = Posting.objects.all()[0]
+        response     = client.delete(f"/galleries/{gallery.id}/{posting.id}", **headers)
+
+        self.assertEqual(response.status_code, 204)
+
+class CommentTest(TestCase) :
+    @classmethod
+    def setUpTestData(self) :
+        gallery = Gallery.objects.create(
+            name  = "test",
+            image = "image.jpg"
+        )
+
+        User.objects.bulk_create([
+            User(nickname = "testuser1"),
+            User(nickname = "testuser2")
+        ]
+        )
+
+        Posting.objects.bulk_create([
+            Posting(
+                gallery = gallery,
+                title   = "testpost1",
+                content = "testtext1",
+                user    = User.objects.get(nickname = "testuser1")
+            ),
+            Posting(
+                gallery = gallery,
+                title   = "testpost2",
+                content = "testtext2",
+                user    = User.objects.get(nickname = "testuser1")
+            ),
+            Posting(
+                gallery = gallery,
+                title   = "testpost3",
+                content = "testtext3",
+                user    = User.objects.get(nickname = "testuser1")
+            ),
+            Posting(
+                gallery = gallery,
+                title   = "testpost3",
+                content = "testtext3",
+                user    = User.objects.get(nickname = "testuser2")
+            )
+        ])
+
+        Comment.objects.bulk_create([
+            Comment(
+                user    = User.objects.get(nickname = "testuser2"),
+                posting = Posting.objects.get(title = "testpost1")
+            ),
+            Comment(
+                user    = User.objects.get(nickname = "testuser1"),
+                posting = Posting.objects.get(title = "testpost1")
+            ),
+            Comment(
+                user    = User.objects.get(nickname = "testuser1"),
+                posting = Posting.objects.get(title = "testpost2")
+            ),
+            Comment(
+                user    = User.objects.get(nickname = "testuser1"),
+                posting = Posting.objects.get(title = "testpost2")
+            ),
+        ])
+
+    def tearDown(self) :
+        Comment.objects.all().delete()
+        Posting.objects.all().delete()
+        User.objects.all().delete()
+        Gallery.objects.all().delete()
+
+    def test_comment_read_success(self) :
+        client     = Client()
+        gallery_id = Gallery.objects.all()[0].id
+        posting_id = Posting.objects.all()[0].id
+        response   = client.get(f"/galleries/{gallery_id}/{posting_id}/comments")
+        self.assertEqual(response.status_code, 200)
+
+    def test_comment_post_success(self) :
+        client = Client()
+        user_id = User.objects.all()[0].id
+        access_token = jwt.encode({'id' : user_id}, SECRET_KEY, algorithm = ALGORITHMS)
+        headers = {"HTTP_Authorization" : access_token}
+        gallery = Gallery.objects.all()[0]
+        posting = Posting.objects.all()[0]
+        body = {"content" : "4123"}
+        response = client.post(f"/galleries/{gallery.id}/{posting.id}/comments", json.dumps(body),
+                               content_type = 'application/json', **headers)
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_comment_patch_success(self) :
+        client       = Client()
+        user_id      = User.objects.all()[0].id
+        access_token = jwt.encode({'id' : user_id}, SECRET_KEY, algorithm = ALGORITHMS)
+        headers      = {"HTTP_Authorization" : access_token}
+        gallery      = Gallery.objects.all()[0]
+        posting      = Posting.objects.all()[0]
+        comment      = Comment.objects.get(posting_id = posting.id, user_id = user_id)
+        body         = {"content" : "4124"}
+        response     = client.patch(f"/galleries/{gallery.id}/{posting.id}/comments/{comment.id}", json.dumps(body),
+                                content_type = 'application/json', **headers)
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_comment_delete_success(self) :
+        client       = Client()
+        user_id      = User.objects.all()[0].id
+        access_token = jwt.encode({'id' : user_id}, SECRET_KEY, algorithm = ALGORITHMS)
+        headers      = {"HTTP_Authorization" : access_token}
+        gallery      = Gallery.objects.all()[0]
+        posting      = Posting.objects.all()[0]
+        comment      = Comment.objects.get(posting_id = posting.id, user_id = user_id)
+        response     = client.delete(f"/galleries/{gallery.id}/{posting.id}/comments/{comment.id}", **headers)
+
+        self.assertEqual(response.status_code, 204)
--- galleries/views.py
+++ galleries/views.py
@@ -1,7 +1,10 @@
-from django.http        import JsonResponse
-from django.views       import View
+import json
 
-from galleries.models   import Gallery, Bookmark
+from django.core.paginator import Paginator
+from django.views          import View
+from django.http           import JsonResponse
+
+from galleries.models   import Gallery, Bookmark, Posting, Comment
 from users.utils        import login_decorator
 
 class BookmarkView(View):
@@ -31,3 +34,167 @@ class BookmarkView(View):
         } for bookmark in bookmarks ]
 
         return JsonResponse({'LIST' : gallery_list}, status=200)
+
+class PostingsView(View):
+    def get(self, request, gallery_id):
+        if not Gallery.objects.filter(id=gallery_id).exists():
+            return JsonResponse({"MESSAGE": "KEY_ERROR"}, status = 400)
+
+        postingslist = Posting.objects.filter(gallery=gallery_id).select_related("user")\
+            .prefetch_related("comment_set").order_by("created_at")
+
+        pagenator = Paginator(postingslist, 10)
+        page      = request.GET.get("page", 1)
+        postings  = pagenator.get_page(page)
+        is_next   = postings.has_next()
+
+        response = [{
+            "id"            : posting.id,
+            "title"         : posting.title,
+            "thumbnail"     : posting.thumbnail,
+            "view_count"    : posting.view_count,
+            "created_at"    : posting.created_at,
+            "updated_at"    : posting.updated_at,
+            "comment_count" : posting.comment_set.count(),
+            "user_nickname" : posting.user.nickname,
+            "user_id"       : posting.user.id,
+        } for posting in postings]
+
+        return JsonResponse({"MESSAGE" : response, "is_next" : is_next}, status=200)
+
+    @login_decorator
+    def post(self, request, gallery_id) :
+        try :
+            if not Gallery.objects.filter(id = gallery_id).exists() :
+                return JsonResponse({"MESSAGE" : "NO_GALLERY"}, status = 404)
+
+            data = json.loads(request.body)
+            Posting.objects.create(
+                gallery_id = gallery_id,
+                title      = data.get("title"),
+                content    = data.get("content"),
+                user       = request.user,
+                thumbnail  = data.get("thumbnail")
+            )
+
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 201)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+
+class PostingView(View):
+    def get(self, request, posting_id, gallery_id):
+        if not Posting.objects.filter(id = posting_id).exists():
+            return JsonResponse({"MESSAGE": "KEYERROR"}, status = 400)
+
+        posting = Posting.objects.select_related("user").prefetch_related("comment_set").get(id = posting_id)
+
+        posting.view_count += 1
+        posting.save()
+
+        response = {
+            "id"            : posting.id,
+            "title"         : posting.title,
+            "thumbnail"     : posting.thumbnail,
+            "view_count"    : posting.view_count,
+            "created_at"    : posting.created_at,
+            "updated_at"    : posting.updated_at,
+            "comment_count" : posting.comment_set.count(),
+            "user_name"     : posting.user.nickname,
+            "user_id"       : posting.user.id
+        }
+
+        return JsonResponse({"MESSAGE" : response}, status = 200)
+
+    @login_decorator
+    def patch(self, request, posting_id, gallery_id) :
+        if Posting.objects.get(id = posting_id).user != request.user :
+            return JsonResponse({"MESSAGE" : "NO_PERMISSION"}, status = 403)
+
+        try :
+            data = json.loads(request.body)
+            Posting.objects.filter(id = posting_id).update(
+                title     = data.get("title"),
+                content   = data.get("content"),
+                thumbnail = data.get("thumbnail")
+            )
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 201)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+
+    @login_decorator
+    def delete(self, request, posting_id, gallery_id) :
+        if Posting.objects.get(id = posting_id).user != request.user :
+            return JsonResponse({"MESSAGE" : "NO_PERMISSION"}, status = 403)
+
+        try :
+            Posting.objects.filter(id = posting_id).delete()
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 204)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+
+class CommentsView(View):
+    def get(self, request, posting_id, gallery_id):
+        commentslist = Comment.objects.filter(posting = posting_id).select_related("user").order_by("created_at")
+
+        pagenator  = Paginator(commentslist, 10)
+        page       = request.GET.get("page", 1)
+        comments   = pagenator.get_page(page)
+        num_pages  = pagenator.num_pages
+
+        response = [{
+            "id"            : comment.id,
+            "content"       : comment.content,
+            "created_at"    : comment.created_at,
+            "updated_at"    : comment.updated_at,
+            "user_nickname" : comment.user.nickname,
+            "user_id"       : comment.user.id
+        } for comment in comments]
+
+        return JsonResponse({"MESSAGE" : response, "page_range" : num_pages}, status = 200)
+
+    @login_decorator
+    def post(self, request, posting_id, gallery_id) :
+        try :
+            if not Posting.objects.filter(id = posting_id).exists() :
+                return JsonResponse({"MESSAGE" : "NO_POSTING"}, status = 404)
+
+            data = json.loads(request.body)
+            Comment.objects.create(
+                content = data.get("content"),
+                user = request.user,
+                posting_id = posting_id
+            )
+
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 201)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+
+class CommentView(View):
+    @login_decorator
+    def patch(self, request, posting_id, gallery_id, comment_id):
+        if Comment.objects.get(id=comment_id).user != request.user:
+            return JsonResponse({"MESSAGE" : "NO_PERMISSION"}, status = 403)
+
+        try :
+            data = json.loads(request.body)
+            Comment.objects.filter(id=comment_id).update(content = data.get("content"))
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 201)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
+
+    @login_decorator
+    def delete(self, request, posting_id, gallery_id, comment_id) :
+        if Posting.objects.get(id = comment_id).user != request.user:
+            return JsonResponse({"MESSAGE" : "NO_PERMISSION"}, status = 403)
+
+        try :
+            Comment.objects.filter(id=comment_id).delete()
+            return JsonResponse({"MESSAGE" : "SUCCESS"}, status = 204)
+
+        except KeyError :
+            return JsonResponse({"message" : "KEY_ERROR"}, status = 400)
