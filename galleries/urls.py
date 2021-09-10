4e4cd2cab4ffa1820346bd0bb9eaadf05a056f44
dwenpark <dwenup@gmail.com>
2021-09-10 11:26:52 +0900

    Add: 게시글 리스트, 게시글, 댓글 CRUD
    - 게시글 리스트
    - 게시글 보기, 쓰기, 수정, 삭제
    - 댓글 보기, 쓰기, 수정, 삭제
    - 테스트 코드 작성

--- galleries/urls.py
+++ galleries/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from galleries.views import BookmarkView
+from galleries.views import BookmarkView, PostingsView, PostingView, CommentsView, CommentView
 
 urlpatterns = [
     path('/<int:gallery_id>/bookmark', BookmarkView.as_view()),
-    path('/bookmark-list', BookmarkView.as_view())
+    path('/bookmark-list', BookmarkView.as_view()),
+    path("/<int:gallery_id>", PostingsView.as_view()),
+    path("/<int:gallery_id>/<int:posting_id>", PostingView.as_view()),
+    path("/<int:gallery_id>/<int:posting_id>/comments", CommentsView.as_view()),
+    path("/<int:gallery_id>/<int:posting_id>/comments/<int:comment_id>", CommentView.as_view()),
 ]
