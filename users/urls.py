e978b489429413ec03988c1bc18a851057034c69
최민 <cm9542@gmail.com>
2021-09-28 22:14:58 +0900

    Add: Profile 보기 기능 추가

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import NamecardView, KakaoLoginView, NicknameView, MyPostingsView
+from users.views import NamecardView, KakaoLoginView, NicknameView, MyPostingsView, ProfileView
 
 urlpatterns = [
     path('/namecard', NamecardView.as_view()),
     path('/kakao', KakaoLoginView.as_view()),
     path('/nickname', NicknameView.as_view()),
     path('/postings', MyPostingsView.as_view()),
+    path('/<int:user_id>/profile', ProfileView.as_view()),
 ]
